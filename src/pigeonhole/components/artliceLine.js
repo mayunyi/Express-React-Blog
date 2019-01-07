342a7d0f5956ee589423a356fa0189902116ebbc
mayunyi <952359947@qq.com>
2019-01-07 17:55:17 +0800

    文章时间轴

--- src/pigeonhole/components/artliceLine.js
+++ src/pigeonhole/components/artliceLine.js
@@ -3,9 +3,10 @@
  * 文章时间轴线
  */
 import React, { Component } from 'react';
-import { Timeline,Pagination  } from 'antd';
+import { Timeline,Pagination ,message } from 'antd';
 import {Link} from 'react-router-dom'
 import '../styles/index.css'
+import { getUser } from '../../auth'
 
 
 
@@ -14,26 +15,41 @@ export default class ArtliceLine extends Component {
 
     constructor(props,context){
         super(props,context);
+        this.user = getUser();
         this.state ={
             allArtlice:[],
             total:0,
-            page:1
+            page:1,
+            rows:20
         }
     }
     componentDidMount(){
-        this.getAllArticle(this.state.page);
+        const { page, rows } = this.state;
+        this.getAllArticle(page,rows);
     }
-    getAllArticle(value){
-        fetch(`/blog/page/article/all/${value}`).then(rep=>{
+    getAllArticle(page,rows){
+        if(!this.user.userId){
+            return message.warning('请登录！')
+        }
+        fetch(
+            `/api/articlelist/user?page=${page}&rows=${rows}&userid=${this.user.userId}`,
+            {
+                method: "GET",
+                mode: "cors",
+                headers: {
+                    "Authorization": this.user.token
+                },
+            }
+        ).then(rep=>{
             return rep.json();
         }).then(json=>{
-            let articleData = json.res.rows;
+            let articleData = json.data;
             articleData.sort((a,b)=>{
-                return  Date.parse(b.createdAt) - Date.parse(a.createdAt)
+                return  Date.parse(b.date) - Date.parse(a.date)
             });
             this.setState({
                 allArtlice:articleData,
-                total:json.res.count
+                total:json.toatl
             })
         })
     }
@@ -41,14 +57,14 @@ export default class ArtliceLine extends Component {
         this.setState({
             page:page
         });
-        this.getAllArticle(page)
+        this.getAllArticle(page,this.state.rows)
     };
     render(){
         let allArtlice = this.state.allArtlice;
         let TimeLine=[];
         let timeData=[];
         for(let i=0;i<allArtlice.length;i++){
-            let time = allArtlice[i].createdAt?allArtlice[i].createdAt.split('T')[0]:'';
+            let time = allArtlice[i].date ? allArtlice[i].date.split('T')[0]:'';
             if(timeData.indexOf(time) === -1){
                 timeData.push(time)
             }
@@ -66,18 +82,18 @@ export default class ArtliceLine extends Component {
             }
 
             allArtlice.map(item=>{
-                if(item.createdAt){
-                    if(s === item.createdAt.split('T')[0]){
+                if(item.date){
+                    if(s === item.date.split('T')[0]){
                         TimeLine.push(
-                            <Timeline.Item key = {item.articleId}>
-                                <Link to={`/artile/${item.articleId}`}>{item.articleTitle}</Link>
+                            <Timeline.Item key = {item._id}>
+                                <Link to={`/artile/${item._id}`}>{item.Title}</Link>
                             </Timeline.Item>
                         )
                     }
                 } else {
                     TimeLine.push(
-                        <Timeline.Item key = {item.articleId}>
-                            <Link to={`/artile/${item.articleId}`}>{item.articleTitle}</Link>
+                        <Timeline.Item key = {item._id}>
+                            <Link to={`/artile/${item._id}`}>{item.Title}</Link>
                         </Timeline.Item>
                     )
                 }
@@ -103,7 +119,7 @@ export default class ArtliceLine extends Component {
 
                 <div className="pageLine">
                     {
-                        this.state.total != 0 && <Pagination onChange={this.onChange} total={this.state.total} defaultPageSize={20} current={this.state.page}/>
+                        this.state.total != 0 && <Pagination onChange={this.onChange} total={this.state.total} defaultPageSize={this.state.rows} current={this.state.page}/>
                     }
                 </div>
             </div>
